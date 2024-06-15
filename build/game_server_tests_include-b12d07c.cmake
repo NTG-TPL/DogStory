39a15f94fb013834cadb74a12b272d4d1391477b
NTG-TPL <nikolay.tep@yandex.ru>
2024-06-15 21:15:04 +0300

    fix build

--- build/game_server_tests_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/ntg/github_projects/DogStory/build/game_server_tests_tests-b12d07c.cmake")
-  include("/home/ntg/github_projects/DogStory/build/game_server_tests_tests-b12d07c.cmake")
-else()
-  add_test(game_server_tests_NOT_BUILT-b12d07c game_server_tests_NOT_BUILT-b12d07c)
-endif()
